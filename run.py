f33dbbbf8105e5ec2e374472493bdfb87202e64c
Bood Qian <bood@glowing.com>
2018-02-23 21:47:03 +0800

    Test multiple times for ladder tests

--- run.py
+++ run.py
@@ -22,6 +22,10 @@ PASS_STATUS = "%s[PASS]%s" % (bcolors.OKGREEN, bcolors.ENDC)
 FAIL_STATUS = "%s[FAIL]%s" % (bcolors.FAIL, bcolors.ENDC)
 
 
+MULTI_RUN_GROUPS = ("ladder")
+DEFAULT_MULTI_RUNS = 10
+
+
 def my_print(message):
     print(message, end="")
 
@@ -29,6 +33,16 @@ def my_print(message):
 def print_status(moves, is_pass):
     message = ' '.join(moves) if isinstance(moves, list) else str(moves)
     print("%s %s" % (PASS_STATUS if is_pass else FAIL_STATUS, message))
+
+
+def print_multi_status(results):
+    pass_num = sum(x for x in results)
+    color = bcolors.WARNING
+    if pass_num == len(results):
+        color = bcolors.OKGREEN
+    elif pass_num == 0:
+        color = bcolors.FAIL
+    print("%s[%s/%s PASSES]%s" % (color, pass_num, len(results), bcolors.ENDC))
 
 
 def debug(message):
@@ -41,6 +55,29 @@ def find(values, value):
         return values.index(value)
     except ValueError:
         return -1
+
+
+def do_single_test(test):
+    gtp = "loadsgf ./sgf/%s\ngenmove %s" % (test['sgf'], test['move'])
+    lines = subprocess.check_output("echo '%s' | %s 2>&1 | egrep -- '^\s+[A-Z][0-9]+ +->'" % (gtp, command), shell=True)
+    line = subprocess.check_output("echo '%s' | head -1 | tr -d '[:cntrl:]'" % lines, shell=True)
+    debug("%s\n" % line)
+
+    match = re.search('\(V: (\d+\.\d+)%\).+PV: (.+)', line)
+    win_rate = float(match.group(1))
+    moves = match.group(2).split(' ')
+    next_move = moves[0]
+
+    if test.get('yes_move'):
+        yes_moves = [m.upper() for m in test['yes_move']]
+        return (line, next_move in yes_moves)
+    elif test.get('no_move'):
+        no_moves = [m.upper() for m in test['no_move']]
+        return (line, next_move not in no_moves)
+    elif test.get('max_win_rate'):
+        return (line, win_rate <= float(test['max_win_rate']))
+    else:
+        raise Exception("Neither yes_move, no_move or win_rate found")
 
 
 parser = argparse.ArgumentParser(description='Scenario testing tool for Go')
@@ -66,24 +103,14 @@ for test in tests:
     if args.case and test['sgf'] not in args.case:
         continue
     my_print("%s\n" % test['sgf'])
-    gtp = "loadsgf ./sgf/%s\ngenmove %s" % (test['sgf'], test['move'])
-    debug("%s\n" % gtp)
-    lines = subprocess.check_output("echo '%s' | %s 2>&1 | egrep -- '^\s+[A-Z][0-9]+ +->'" % (gtp, command), shell=True)
-    debug("%s\n" % lines)
-    line = subprocess.check_output("echo '%s' | head -1 | tr -d '[:cntrl:]'" % lines, shell=True)
-
-    match = re.search('\(V: (\d+\.\d+)%\).+PV: (.+)', line)
-    win_rate = float(match.group(1))
-    moves = match.group(2).split(' ')
-    next_move = moves[0]
-
-    if test.get('yes_move'):
-        yes_moves = [m.upper() for m in test['yes_move']]
-        print_status(line, next_move in yes_moves)
-    elif test.get('no_move'):
-        no_moves = [m.upper() for m in test['no_move']]
-        print_status(line, next_move not in no_moves)
-    elif test.get('max_win_rate'):
-        print_status(line, win_rate <= float(test['max_win_rate']))
+    if test['group'] in MULTI_RUN_GROUPS:
+        results = []
+        for i in xrange(0, DEFAULT_MULTI_RUNS):
+            my_print("%s " % i)
+            (line, result) = do_single_test(test)
+            results.append(result)
+        my_print("\n")
+        print_multi_status(results)
     else:
-        raise Exception("Neither yes_move, no_move or win_rate found")
+        (line, result) = do_single_test(test)
+        print_status(line, result)
